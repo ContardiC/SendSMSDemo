2c6a46870ef0d1153fee345931dbb23f56626e1f
Carlo Contardi <c.contardi@spacecoding.it>
2025-02-23 11:43:37 +0100

    [Feature] Add: send sms

--- app/src/main/java/it/spacecoding/sendsmsdemo/MainActivity.java
+++ app/src/main/java/it/spacecoding/sendsmsdemo/MainActivity.java
@@ -1,24 +1,60 @@
 package it.spacecoding.sendsmsdemo;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText message, number;
+    Button send;
+    String userMessage;
+    String userNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        send = findViewById(R.id.buttonSend);
+        message = findViewById(R.id.editTextMessage);
+        number = findViewById(R.id.editTextNumber);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userMessage = message.getText().toString();
+                userNumber = number.getText().toString();
+                sendSMS(userMessage, userNumber);
+            }
         });
+
+
+    }
+    public void sendSMS(String userMessage, String userNumber){
+        if(ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{ android.Manifest.permission.SEND_SMS}, 1);
+
+        }else{
+            // TODO: a message for the user
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+        if(requestCode == 1 && grantResults.length >0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(userNumber, null, userMessage, null, null);
+        }
     }
 }
